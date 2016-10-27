2a83989d2c8decee03e0e5db7110007401fec199
Denys Mikhalenko <denys@prontiol.com>
2016-10-27 15:55:28 +0800

    Add `firebase` boilerplate

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase'
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyCK0DZNLUB5TIlihx_5EoZ6Gqbyk2-FdVc',
+  authDomain: 'placespace-47a03.firebaseapp.com',
+  databaseURL: 'https://placespace-47a03.firebaseio.com',
+  messagingSenderId: '933537365603',
+  storageBucket: 'placespace-47a03.appspot.com',
+})
+
+export function syncFirebase(store) {
+}
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from '../reducers'
+import { syncFirebase } from '../firebase'
 
 export default function configureStore(initialState) {
   const store = createStore(
     (state) => state,
     applyMiddleware(thunk)
   )
+  syncFirebase(store)
 
   if (module.hot) {
     module.hot.accept(() => {
