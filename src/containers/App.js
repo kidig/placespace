4b4605ad02a20222ec66788477f076a339f017ab
Denys Mikhalenko <denys@prontiol.com>
2016-11-29 14:21:34 +0800

    Debug representation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+
+const style = StyleSheet.create({
+  root: {
+    backgroundColor: 'orange',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    lineHeight: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  version: {
+    fontSize: 11,
+    color: 'white',
+  },
+})
 
 class App extends Component {
 
   render() {
     return (
-      <View/>
+      <View style={style.root}>
+        <Text style={style.title}>PlaceSpace</Text>
+        <Text style={style.version}>0.0.1</Text>
+      </View>
     )
   }
 }
