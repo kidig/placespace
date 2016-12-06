71447ad3be86ea3df49de98ffbc6d8457b3e40de
Denys Mikhalenko <denys@prontiol.com>
2016-12-06 16:31:50 +0800

    Add `CodePush`

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
+import codePush from 'react-native-code-push'
 import App from './containers/App'
 import configureStore from './store/configureStore'
 
 const store = configureStore()
+
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+}
 
 class PlaceSpace extends Component {
 
@@ -18,4 +24,5 @@ class PlaceSpace extends Component {
 
 }
 
-AppRegistry.registerComponent('PlaceSpace', () => PlaceSpace)
+
+AppRegistry.registerComponent('PlaceSpace', () => codePush(codePushOptions)(PlaceSpace))
